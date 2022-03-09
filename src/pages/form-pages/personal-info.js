e2c42b3e944d49d570c201e22ad88ec0ddc286f5
Gio Atanelishvili <gatanelishvili@gmail.com>
2022-03-09 10:36:24 +0400

    add styles and functionality to personal info page

--- src/pages/form-pages/personal-info.js
+++ src/pages/form-pages/personal-info.js
@@ -1,5 +1,18 @@
 import React from "react";
+import RightPanel from "../../components/RightPanel";
+import "../../stylesheets/form-pages.css";
+
+const PAGE = 1;
 
 export default function PersonalInfo() {
-  return <div></div>;
+  return (
+    <div className="form-page-container">
+      <section className="left-panel">
+        <h1 className="header">Hey, Rocketeer, what are your coordinates?</h1>
+      </section>
+      <section className="right-panel">
+        <RightPanel page={PAGE} />
+      </section>
+    </div>
+  );
 }

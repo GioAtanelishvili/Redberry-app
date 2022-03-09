30c4fddf66da6a296ea3ad2cbcb25d3948e0b019
Gio Atanelishvili <gatanelishvili@gmail.com>
2022-03-09 09:47:35 +0400

    add styles and functionality to home page

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,5 +1,23 @@
 import React from "react";
+import rocketman from "../images/rocketman.png";
+import "../stylesheets/home.css";
 
 export default function Home() {
-    return <div></div>
+  return (
+    <div className="home-page-container">
+      <h1 className="greeting-message">Welcome Rocketeer!</h1>
+      <div className="home-buttons-container">
+        <button
+          className="start-button"
+          onClick={() => {
+            window.open("/personal-info", "_self");
+          }}
+        >
+          Start Questionarrie
+        </button>
+        <a href="/submitted-forms">Submitted applications</a>
+      </div>
+      <img src={rocketman} className="rocketman-img" alt="Rocketman" />
+    </div>
+  );
 }
